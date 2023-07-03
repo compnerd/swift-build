5279570b7d30ab8ba457c228c4810339b32fecc7
Hiroshi Yamauchi <hjyamauchi@gmail.com>
2023-07-03 12:16:51 -0700

    Fix the ARM64-on-ARM64 build for icu.

--- build.ps1
+++ build.ps1
@@ -786,7 +786,7 @@ function Build-ICU($Arch) {
     }
   }
 
-  if ($Arch -eq $ArchARM64) {
+  if ($Arch -eq $ArchARM64 -and $HostArch -eq $ArchX64) {
     # Use previously built x64 tools
     $BuildToolsDefines = @{
       BUILD_TOOLS = "NO";
