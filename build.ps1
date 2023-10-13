d92f4a5d17096cb2415adc2cac7ec9bb99ed4ec3
Tristan Labelle <tristan@thebrowser.company>
2023-10-13 15:24:00 -0400

    Remove extra property set

--- build.ps1
+++ build.ps1
@@ -1558,7 +1558,6 @@ function Build-Installer() {
     INCLUDE_SWIFT_DOCC = "true";
     SWIFT_DOCC_BUILD = "$($HostArch.BinaryCache)\swift-docc\release";
     SWIFT_DOCC_RENDER_ARTIFACT_ROOT = "${SourceCache}\swift-docc-render-artifact";
-    VCREDIST_INSTALLER = "$($env:VCToolsRedistDir)"
   }
 
   Isolate-EnvVars {
